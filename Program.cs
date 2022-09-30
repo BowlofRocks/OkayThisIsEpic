06502436361912bb71948c927f1025683d9aa49c
Paul Amago <apaulo2035@gmail.com>
2022-09-30 08:20:29 -0600

    this saturday's project

--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
-﻿using System;
-
-
-namespace Test
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-
-    }
-    
-} 
+﻿
